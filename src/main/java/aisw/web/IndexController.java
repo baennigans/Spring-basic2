ada7ce7c289aceec94704da8a13692576f5e2240
Bae GunHee <bgh2584@gmail.com>
2023-07-13 16:26:52 +0900

    NewsService

--- src/main/java/aisw/web/IndexController.java
+++ src/main/java/aisw/web/IndexController.java
@@ -34,4 +34,25 @@ public class IndexController {
 //		return mv;
 //	}
 
+	public List<News> getNewList() ... {
+		// 뉴스 정보 받아올 url 입력
+		String url = "http://172.31.9.169:8080/index";
+		//RestTemplate으로 JSON 결과값 받아오기
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.exchange(url,  HttpMethod.GET, null, String.class);
+		// 결과값 중 JSON 데이터만 가져와기
+		String responseBody = null;
+		if (response.getStatusCode().is2xxSuccessful()) {
+			responseBody = response.getBody();
+			System.out.println(responseBody);
+		} else {
+			System.out.println("fail : " + response.getStatusCode());
+		}
+		// JSON 데이터를 VO로 mapping하기 (상대방 JSON 데이터에 맞는 VO를 생성해야 함
+		List<News> newsList = null;
+		ObjectMapper mapper = new ObjectMapper();
+		newsList = mapper.readValue(responseBody, new TypeReference<List<News>>() {});
+						
+		return newsList;
+		}	
 }
